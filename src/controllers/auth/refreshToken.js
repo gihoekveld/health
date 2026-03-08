df518808180235ce2556c68868f5d59d4c9c91d8
Giselle Hoekveld <gisellehoekveld@gmail.com>
2026-03-08 16:15:07 -0300

    feat: enhance refresh token functionality with new token generation and cookie handling

--- src/controllers/auth/refreshToken.js
+++ src/controllers/auth/refreshToken.js
@@ -21,7 +21,24 @@ export const refreshToken = (req, res) => {
       }
     );
 
-    return res.status(200).json({ token: newToken });
+    const newRefreshToken = jwt.sign(
+      { user: { id: payload.user.id, email: payload.user.email } },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: '7d', // Novo refresh token expira em 7 dias
+      }
+    );
+
+    res.cookie('refreshToken', newRefreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 dias
+    });
+
+    return res
+      .status(200)
+      .json({ token: newToken, refreshToken: newRefreshToken });
   } catch (err) {
     console.error('Ocorreu um erro ao verificar o refresh token:', err);
 
